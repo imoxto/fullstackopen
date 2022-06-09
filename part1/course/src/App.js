a67792bdc29711fd2c65af5d1ac920c74a2a870a
xImouto <mohammad.rafid.hamid@g.bracu.ac.bd>
2022-06-09 20:27:59 +0600

    ex1.1-1.2

--- part1/course/src/App.js
+++ part1/course/src/App.js
@@ -1,13 +1,46 @@
-function App() {
+import React from "react";
+
+function Part({ part, exer }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={`https://images-ext-1.discordapp.net/external/UxndLb6bovlEcaAvQu2J7bHp9TE3y-lrin0hRk9KEm0/https/cdn.discordapp.com/avatars/219543070352343043/e03a49f0f8f91f2a2e6299be7d92e2bc.webp`} className="App-logo" alt="logo" />
-        <p>
-          Learn React
-        </p>
-      </header>
-    </div>
+    <p>
+      {part} {exer}
+    </p>
+  );
+}
+
+function Header({ header }) {
+  return <h1>{header}</h1>;
+}
+
+function Content({ part1, exer1, part2, exer2, part3, exer3 }) {
+  return (
+    <>
+      <Part part={part1} exer={exer1} />
+      <Part part={part2} exer={exer2} />
+      <Part part={part3} exer={exer3} />
+    </>
+  );
+}
+
+function Total({ num }) {
+  return <p>Number of exercises {num}</p>;
+}
+
+function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <>
+      <Header header={course} />
+      <Content part1={part1} part2={part2} part3={part3} exer1={exercises1} exer2={exercises2} exer3={exercises3} />
+      <Total num={exercises1 + exercises2 + exercises3} />
+    </>
   );
 }
 
