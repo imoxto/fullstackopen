afa04bbbf593d9f1d5be87e5facbc4a014a4e64a
xImouto <mohammad.rafid.hamid@g.bracu.ac.bd>
2022-06-09 18:58:14 +0600

    setup environment

---
+++ part1/course/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={`https://images-ext-1.discordapp.net/external/UxndLb6bovlEcaAvQu2J7bHp9TE3y-lrin0hRk9KEm0/https/cdn.discordapp.com/avatars/219543070352343043/e03a49f0f8f91f2a2e6299be7d92e2bc.webp`} className="App-logo" alt="logo" />
+        <p>
+          Learn React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
